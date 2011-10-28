5f5079290b129c1d62378a87bba53fa605523e2c
Paolo Castagna <paolo.castagna@gmail.com>
2011-10-28 10:39:41 +0100

    Added missing headers.

--- src/test/java/dev/TDBVerifier.java
+++ src/test/java/dev/TDBVerifier.java
@@ -1,6 +1,22 @@
-package dev;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import static org.junit.Assert.assertEquals;
+package dev;
 
 import java.io.File;
 import java.nio.ByteBuffer;
@@ -69,7 +85,10 @@ public class TDBVerifier {
 		System.out.println("Nodes.dat file size is: " + new File(filename).length());
 		long filesize = nodeId + nodeValue.capacity() + SystemTDB.SizeOfInt;
 		System.out.println(nodeId + " + " + nodeValue.capacity() + " + " + SystemTDB.SizeOfInt + " = " + filesize);
-		assertEquals (new File(filename).length(), filesize);
+		
+		if ( new File(filename).length() != filesize ) {
+			error("The nodes.dat has a problem!");
+		}
 		
 		return nodeIds;
 	}
@@ -109,12 +128,20 @@ public class TDBVerifier {
 			System.out.println("Found " + countRecords + " records.");
 
 		} catch (StorageException e) {
+			System.out.println("------------------------------------------------------------------------------");
 			e.printStackTrace(System.out);
+			System.out.println("------------------------------------------------------------------------------");
 		}
 
 
 	}
 
+	private static void error(String msg) {
+		System.out.println("------------------------------------------------------------------------------");
+		System.out.println(msg);
+		System.out.println("------------------------------------------------------------------------------");
+	}
+	
 	private static void print_usage() {
 		System.out.println("Usage: TDBVerifier <path>");
 		System.exit(0);		
